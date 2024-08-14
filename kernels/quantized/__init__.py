2e10a7285e21f604cba09fb2efdd0c0cf2f3e512
Mengwei Liu <larryliu@meta.com>
2024-08-14 11:44:25 -0700

    Fix
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- kernels/quantized/__init__.py
+++ kernels/quantized/__init__.py
@@ -7,21 +7,15 @@
 try:
     from pathlib import Path
 
-    quantized_ops_aot_lib = list(Path(__file__).parent.resolve().glob("**/libquantized_ops_aot_lib.*"))
-    # embedding_ops_aot_lib = list(Path(__file__).parent.resolve().glob("**/libembedding_ops_aot_lib.*"))
+    libs = list(Path(__file__).parent.resolve().glob("**/libquantized_ops_aot_lib.*"))
     del Path
-    assert len(quantized_ops_aot_lib) == 1, f"Expected 1 library but got {len(quantized_ops_aot_lib)}"
-    # assert len(embedding_ops_aot_lib) == 1, f"Expected 1 library but got {len(embedding_ops_aot_lib)}"
+    assert len(libs) == 1, f"Expected 1 library but got {len(libs)}"
     import torch as _torch
 
-    _torch.ops.load_library(quantized_ops_aot_lib[0])
-    op = torch.ops.quantized.add_out
-    assert op is not None
-    # _torch.ops.load_library(embedding_ops_aot_lib[0])
-
+    _torch.ops.load_library(libs[0])
     del _torch
 except:
     import logging
 
-    logging.info("libquantized_ops_aot_lib and/or libembedding_ops_aot_lib is not loaded")
+    logging.info("libquantized_ops_aot_lib is not loaded")
     del logging
